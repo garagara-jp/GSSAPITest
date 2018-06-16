2810609825cde4940c3559c0e0a8d0d02f435954
garagara-jp <kramschrott@gmail.com>
2018-06-17 08:46:42 +0900

    firrrrrrrst commit

---
+++ GSSTest/GSSFormatter.cs
@@ -0,0 +1,76 @@
+﻿/**
+Copyright (c) 2018 garagara-jp
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GSSTest
+{
+    public class GSSFormatter
+    {
+        public static string CreateJson(string originalJson)
+        {
+            // 01.APIで取得したJsonをパース
+            JsonNode tmpJson = JsonNode.Parse(originalJson)["values"];
+
+            // 02.取得したJsonからKeyの名前を取得
+            var keyNameList = new List<string>();
+            foreach (var key in tmpJson[0])
+            {
+                string keyName = key.Get<string>();
+                keyNameList.Add(keyName);
+            }
+
+            // 03.各オブジェクトのパラメータDictionaryをリストに格納する
+            var param = new List<Dictionary<string, object>>();
+            for (int i = 1; i < tmpJson.Count; i++)
+            {
+                // オブジェクトのパラメータを格納するDictionary
+                var valueDic = new Dictionary<string, object>();
+
+                foreach (var key in keyNameList)
+                {
+                    // 取得したJsonからパラメータを取得してDictionaryに格納
+                    var num = keyNameList.IndexOf(key);
+
+                    // string型かint型か区別
+                    var tmp = tmpJson[i][num].Get<string>();
+                    if (int.TryParse(tmp, out int outNum))
+                        valueDic.Add(key, outNum);
+                    else valueDic.Add(key, tmp);
+                }
+
+                // Dictionaryをリストに格納
+                param.Add(valueDic);
+            }
+
+            // 04.Dictionaryを作成してJSON形式にシリアライズ
+            var dic = new Dictionary<string, List<Dictionary<string, object>>>
+            {
+                { "param", param }
+            };
+            var json = JsonConvert.SerializeObject(dic, Formatting.Indented);
+
+            return json;
+        }
+    }
+}
---
+++ GSSTest/Program.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * 以下参照
+ *   Google.Apis,Google.Apis.Auth,Google.Apis.Auth.PlatformServices,
+ *   Google.Apis.Core,Google.Apis.PlatformServices,Google.Apis.Sheets.v4
+ */
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+using Newtonsoft.Json;
+
+namespace GSSTest
+{
+    class Program
+    {
+        private static readonly string CredFileSaveDirectlyName = "google.apis.sheets.v4-Test";
+
+        private static readonly string ApplicationName = "GSS API Test";
+        private static readonly string AppClientId = "my-clientId"; // TODO: Update placeholder value.
+        private static readonly string AppClientSecret = "my-clientSecret"; // TODO: Update placeholder value.
+        private static readonly string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+
+        private static readonly string SpreadSheetId = "my-sheetId";    // TODO: Update placeholder value.
+        private static readonly string Range = "my-range";  // TODO: Update placeholder value.
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Spreadsheet API Test");
+            Console.WriteLine("================================");
+
+            // APIを呼び出してJSONを取得する
+            var originalJson = Task.Run(() => GetJsonAsync());
+            Console.WriteLine("Now Loading...");
+
+            // 取得したJSONを加工する
+            var formattedJson = GSSFormatter.CreateJson(await originalJson);
+            Console.WriteLine($"Original JSON : {await originalJson}");
+            Console.WriteLine();
+            Console.WriteLine($"Formatted JSON : {formattedJson}");
+            Console.WriteLine();
+
+            // 試しに呼び出してみる
+            var jsonNode = JsonNode.Parse(formattedJson);
+            //Console.WriteLine($"浅見 リリスのバストサイズ：{jsonNode["param"][0]["バスト"].Get<Int64>()}");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// APIを呼び出してJSONを取得する
+        /// </summary>
+        private static async Task<string> GetJsonAsync()
+        {
+            // Oauth認証情報のキャッシュするためのディレクトリパスを指定する
+            string credentialSaveToPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            credentialSaveToPath = Path.Combine(credentialSaveToPath, ".credentials/" + CredFileSaveDirectlyName);
+            Console.WriteLine($"Credential file saved to : {credentialSaveToPath}");
+
+            // 認証を実行
+            var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+              new ClientSecrets
+              {
+                  ClientId = AppClientId,
+                  ClientSecret = AppClientSecret
+              },
+              Scopes,
+              "user",
+              CancellationToken.None,
+              new FileDataStore(credentialSaveToPath)
+            );
+
+            Console.WriteLine("Credential is OK");
+
+            // 認証結果を使ってスプレッドシートサービスのインスタンスを作成する
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Requestを投げてセル値のデータを受け取る
+            var request = service.Spreadsheets.Values.Get(SpreadSheetId, Range);
+            var response = await request.ExecuteAsync();
+
+            Console.WriteLine("Response is OK");
+
+            return JsonConvert.SerializeObject(response, Formatting.Indented);
+        }
+    }
+}
